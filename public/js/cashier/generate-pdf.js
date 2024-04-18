01cf257b57b8a862c04b48fde5d2014e3f46663d
Eduardo <picis_9716@hotmail.com>
2024-04-18 13:59:13 -0500

    AGAPE - POLLERIA V.1.0

--- public/js/cashier/generate-pdf.js
+++ public/js/cashier/generate-pdf.js
@@ -29,7 +29,7 @@ $(function () {
                         //aqui podemos programar el print del pdf
                         //parametros para imprimir el pdf de origen
                         //const urlPdf = "https://parzibyte.github.io/plugin-silent-pdf-print-examples/delgado.pdf";
-                        const urlPdf = "https://agape.familc.com/cajera/generate/boleta/pdf/3";
+                        const urlPdf = "https://agape.familc.com/cajera/generate/boleta/pdf/3.pdf";
                         const nombreImpresora = "Microsoft Print to PDF";
                         const url = `http://localhost:8080/url?urlPdf=${urlPdf}&impresora=${nombreImpresora}`;
 
