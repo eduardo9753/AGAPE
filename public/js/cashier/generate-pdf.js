b07b29b689524c1444da81621fd1a15952794d22
Eduardo <picis_9716@hotmail.com>
2024-04-18 17:56:46 -0500

    AGAPE - POLLERIA V.1.0

--- public/js/cashier/generate-pdf.js
+++ public/js/cashier/generate-pdf.js
@@ -40,16 +40,7 @@ $(function () {
                         fetch(url).then(respuesta => {
                             if (respuesta.status === 200) {
                                 alert('datos impresos');
-                                // Descargar el PDF directamente
-                                respuesta.blob().then(blob => {
-                                    const url = window.URL.createObjectURL(blob);
-                                    const a = document.createElement('a');
-                                    a.href = url;
-                                    a.download = 'boleta.pdf';
-                                    document.body.appendChild(a);
-                                    a.click();
-                                    window.URL.revokeObjectURL(url);
-                                });
+                                
                             } else {
                                respuesta.json()
                                .then(mensaje => {
