e67314239774c31dd2ca102c262201961fdf6e35
Eduardo <picis_9716@hotmail.com>
2024-04-21 23:46:01 -0500

    AGAPE - POLLERIA V.1.0

--- resources/views/ticket/pdf/ticket.blade.php
+++ resources/views/ticket/pdf/ticket.blade.php
@@ -14,10 +14,9 @@
         }
 
         .container {
-           
-            margin: 5px auto;
+            margin: 0.5px auto;
             background-color: #fff;
-            padding: 5px;
+            padding: 0.5px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
