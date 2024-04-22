a033392efd9db48f3df4f041d422d050d7d4a8f9
Eduardo <picis_9716@hotmail.com>
2024-04-21 23:06:43 -0500

    AGAPE - POLLERIA V.1.0

--- resources/views/ticket/pdf/ticket.blade.php
+++ resources/views/ticket/pdf/ticket.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orden #{{ $order->id }}</title>
-    <style>
+    {{--<style>
         body {
             font-family: Arial, sans-serif;
             margin: 0;
@@ -89,7 +89,7 @@
         .address p {
             margin: 5px 0;
         }
-    </style>
+    </style>--}}
 </head>
 
 <body>
